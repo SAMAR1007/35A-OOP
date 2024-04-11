acdf0d64af09863e177a67ad1eb02cc77e320f2e
SAMAR1007 <bistsamar999@gmail.com>
2024-04-11 10:42:28 +0500

    new program

--- week1.java
+++ week1.java
@@ -1,5 +1,23 @@
+
+/* 
 public class week1 {
     public static void main(String[] args) {
         System.out.println("hello world");
     }
+}
+
+
+public class week1 {
+
+    public static void main(String[] args) {
+        System.out.println("This is my first program \n i am on module");
+    }
+}
+*/
+
+public class week1 {
+
+public static void main(String[] args) {
+    System.out.println();
+}
 }
